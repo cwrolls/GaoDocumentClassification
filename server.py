2b31e57ebd86434b6b8ea4703ad129271e65f3ff
cwrolls <clairewu2005@live.com>
2024-11-20 00:55:09 -0500

    changed port 8000

--- server.py
+++ server.py
@@ -149,5 +149,5 @@ def get_refresh_token(user_id):
 
 if __name__ == '__main__':
     # app.run(port=8000, debug=True)
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
